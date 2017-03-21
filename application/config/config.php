07a509a4e74f20bbf271194872bcda3f4b59e2e5
oscarsalu <oscarsalu@gmail.com>
2017-03-21 12:54:40 +0300

    link

--- application/config/config.php
+++ application/config/config.php
@@ -451,7 +451,7 @@ $config['global_xss_filtering'] = FALSE;
 $config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
-$config['csrf_expire'] = 7200;
+$config['csrf_expire'] = 1200;
 $config['csrf_regenerate'] = TRUE;
 $config['csrf_exclude_uris'] = array();
 
--- application/views/list.php
+++ application/views/list.php
@@ -1,41 +1,104 @@
-		<div class="content">
+<div class="content">
 				<div class="container-fluid">
 					<div class="row">
+					<?php foreach ($patient as $patient): ?>
 						<div class="col-lg-3 col-md-6 col-sm-6">
 							<div class="card card-stats">
+								<div class="card-header" data-background-color="orange">
+									<i class="material-icons">perm_identity</i>
+								</div>
 								<div class="card-content">
-									<a href="">patient Vitals</a>
+									<p class="category">NAME</p>
+								</div>
+								<div class="card-footer">
+									<div class="stats">
+										<h4><b><?= $patient->first_name; ?></b></h4>
+									</div>
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-3 col-md-6 col-sm-6">
 							<div class="card card-stats">
+								<div class="card-header" data-background-color="green">
+									<i class="material-icons">assessment</i>
+								</div>
 								<div class="card-content">
-									<a href="">Appointments</a>
+									<p class="category">AGE</p>
+								</div>
+								<div class="card-footer">
+									<div class="stats">
+										<h4><?= $patient->age; ?></h4>
+									</div>
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-3 col-md-6 col-sm-6">
 							<div class="card card-stats">
+								<div class="card-header" data-background-color="red">
+									<i class="material-icons">map</i>
+								</div>
 								<div class="card-content">
-									<a href="<?= site_url('services/register') ?>">Services</a>
+									<p class="category">Location</p>
+								</div>
+								<div class="card-footer">
+									<div class="stats">
+										<h4><?= $patient->address; ?></h4>
+									</div>
 								</div>
 							</div>
 						</div>
+
 						<div class="col-lg-3 col-md-6 col-sm-6">
 							<div class="card card-stats">
+								<div class="card-header" data-background-color="blue">
+									<i class="material-icons">contact_phone</i>
+								</div>
 								<div class="card-content">
-									<a href="">Medical Reports</a>
+									<p class="category">Phone Number</p>
+								</div>
+								<div class="card-footer">
+									<div class="stats">
+										<h4><?= $patient->phone; ?></h4>
+									</div>
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-3 col-md-6 col-sm-6">
-							<div class="card card-stats">
-								<div class="card-content">
-									<a href="<?= site_url('payment/index') ?>">Payments</a>
-								</div>
-							</div>
+						<?php endforeach ?>
+					</div>
+					<div class="row">
+					<div class="col-lg-6 col-md-12">
+							<div class="card">
+	                            <div class="card-header" data-background-color="orange">
+	                                <h4 class="title">Vital Sign</h4>
+	                                <p class="category"></p>
+	                            </div>
+	                            <div class="card-content table-responsive">
+	                                <table class="table table-hover">
+	                                    <thead class="text-primary">
+	                                    	<th>Blood Pressure</th>
+	                                    	<th>Oxygen</th>
+	                                    	<th>Pulse Rate</th>
+	                                    	<th>Weight</th>
+											<th>Blood Sugar</th>
+											<th>Date</th>
+	                                    </thead>
+	                                    <tbody>
+	                                        <?php foreach ($results as $data): ?>
+	                                
+	                                	      <tr>
+	                                        	<td><?= $data->BP; ?></td>
+	                                        	<td><?= $data->oxygen; ?></td>
+	                                        	<td><?= $data->pulse; ?></td>
+	                                        	<td><?= $data->weight; ?></td>
+	                                        	<td><?= $data->BS; ?></td>
+												<td><?= $data->date; ?></td>
+	                                        </tr>
+	                                       <?php endforeach ?>
+
+	                                    </tbody>
+	                                </table>
+	                                <p class="lead"><br><?= $links ?><br></p>
+	                            </div>
+	                        </div>
 						</div>
 					</div>
-				</div>
-			</div>
