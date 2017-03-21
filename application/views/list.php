2c9d9ffb1ea9f93e6f351a996818b72fa0c3b30e
oscarsalu <oscarsalu@gmail.com>
2017-03-21 18:00:22 +0300

    list

--- application/views/list.php
+++ application/views/list.php
@@ -1,6 +1,17 @@
-<div class="content">
-				<div class="container-fluid">
-					<div class="row">
+<section >
+                                  <div class="col-md-3">
+                                     <div class="sidebar content-box" style="display: block;">
+                                        <ul class="nav">
+                                            <!-- Main menu -->
+                                            <li class="current"><a href="<?= site_url('auth/index') ?>" <?php if (isset($link_status) && $sub_token=='users'): ?> <?= $active_style ?><?php endif ?>><i class="glyphicon glyphicon-user"></i> Dashboard</a></li>
+                                            <li><a href="<?= site_url('patient/index') ?>" <?php if (isset($link_status) && $sub_token=='patient_search'): ?> <?= $active_style ?><?php endif ?>><i class="fa fa-search"></i> Find Patient</a></li>
+                                            <li><a href="<?= site_url('patient/create_patient') ?>" <?php if (isset($link_status) && $sub_token=='backup'): ?> <?= $active_style ?><?php endif ?>><i class="fa fa-check-circle"></i> Register patient</a></li>
+                            
+                                        </ul>
+                                     </div>
+                                  </div>
+				<div class="col-md-9">
+                     <div class="content-box-large">
 					<?php foreach ($patient as $patient): ?>
 						<div class="col-lg-3 col-md-6 col-sm-6">
 							<div class="card card-stats">
@@ -101,3 +112,6 @@
 	                        </div>
 						</div>
 					</div>
+
+			</div>
+</section>
