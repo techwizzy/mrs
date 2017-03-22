1864264216c1b19ffa60838fa1aca8a3788ed65e
Mugambi Alois <mugambialois@gmail.com>
2017-03-22 11:44:31 +0300

    changes in the view

--- application/views/errors/html/error_db.php
+++ application/views/errors/html/error_db.php
@@ -9,6 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 ::selection { background-color: #E13300; color: white; }
 ::-moz-selection { background-color: #E13300; color: white; }
+::-webkit-selection { background-color: #E13300; color: white; }
 
 body {
 	background-color: #fff;
